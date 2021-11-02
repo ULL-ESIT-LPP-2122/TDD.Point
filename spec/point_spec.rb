4095503b86f3992897b784bfc266f1adc58668cf
Coromoto León <cleon@ull.edu.es>
2021-11-02 19:45:00 +0000

    Debe existir una forma de acceder a las coordenadas de un punto

--- spec/point_spec.rb
+++ spec/point_spec.rb
@@ -8,4 +8,8 @@ describe Point do
     # Define la expectativa
     expect(Point.new(0,0)).not_to eq(nil) #matcher
   end
+  it "Se debe poder acceder a las coordenadas con los getters" do
+    expect(Point.new(0,0).x).to eq(0) 
+    expect(Point.new(0,0).y).to eq(1) 
+  end
 end
