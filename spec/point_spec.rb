ca86f8266a038d6a8a820c14ce3594050801ef80
Coromoto León <cleon@ull.edu.es>
2021-11-02 19:27:00 +0000

    Debe existir una forma de inicializar un punto

--- spec/point_spec.rb
+++ spec/point_spec.rb
@@ -1,4 +1,11 @@
 require 'point'
 
+# Permite agrupar pruebas
 describe Point do
+
+  # Descrbe la prueba 
+  it "Se instancia un punto con coordenadas x, y" do
+    # Define la expectativa
+    expect(Point.new(0,0)).not_to eq(nil) #matcher
+  end
 end
